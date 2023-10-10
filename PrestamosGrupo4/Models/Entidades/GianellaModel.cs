6319f033a46a56476e4949f1bb0a959503ad4858
Giane9296 <136277727+Giane9296@users.noreply.github.com>
2023-10-09 23:24:33 -0500

    creada clase

---
+++ PrestamosGrupo4/Models/Entidades/GianellaModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PrestamosGrupo4.Models.Entidades
+{
+    public class GianellaModel
+    {
+
+        public int idRol { get; set; }
+        public string? nombreRol { get; set; }
+    }
+}
